afb798fd7d2d9ca8612cea03adf5388acd983080
Rafael Camoes <rafaelcamoes@gmail.com>
2023-01-16 20:26:01 -0300

    Currency convertion function v1

--- currencies.py
+++ currencies.py
@@ -1,11 +1,26 @@
-# pylint: disable=missing-docstring
+"This module contains currency convertion functions"
 
-# TODO: add some currency rates
-RATES = {}
+RATES = {"USDEUR": 0.85, "GBPEUR": 1.13, "CHFEUR": 0.86, "EURGBP": 0.885}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-    pass  # YOUR CODE HERE
+    if currency not in ["EUR", "USD", "GBP", "CHF"]:
+        return None
+    if currency == "EUR":
+        if amount[1] == "EUR":
+            # round the value with no decimal places
+            return round(amount[0], 0)
+        if amount[1] == "USD":
+            return round(amount[0] * RATES["USDEUR"], 0)
+        if amount[1] == "GBP":
+            return round(amount[0] * RATES["GBPEUR"], 0)
+        if amount[1] == "CHF":
+            return round(amount[0] * RATES["CHFEUR"], 0)
+    if currency == "GBP":
+        if amount[1] == "EUR":
+            return round(amount[0] * RATES["EURGBP"], 0)
+
+print (convert((100, "EUR"), "GBP"))
